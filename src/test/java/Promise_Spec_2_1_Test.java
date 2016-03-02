b54c1f191d6f10d27c4644606db504752f33102d
Philip Warren <philip.james.warren@gmail.com>
2016-03-02 22:02:42 +1000

    Corrected the test numbering.

--- src/test/java/Promise_Spec_2_1_Test.java
+++ src/test/java/Promise_Spec_2_1_Test.java
@@ -14,16 +14,16 @@ public class Promise_Spec_2_1_Test {
         testObject = new Promise();
     }
 
-    //  2.1
-    //  2.1.1 When pending a promise may transition to the fulfilled state.
-    //  2.1.1 When pending a promise may transition to the rejected state.
+    //  2.1.1
+    //  2.1.1.1 When pending a promise may transition to the fulfilled state.
+    //  2.1.1.1 When pending a promise may transition to the rejected state.
 
-    //  2.2
-    //  2.2.1 When fulfilled a promise must not transition to any other state.
-    //  2.2.2 When fulfilled a promise must have a value which must not change.
+    //  2.1.2
+    //  2.1.2.1 When fulfilled a promise must not transition to any other state.
+    //  2.1.2.2 When fulfilled a promise must have a value which must not change.
 
-    //  2.3
-    //  2.3.1 When rejected a promise must not transition to any other state.
-    //  2.3.2 When rejected a promise must have a reason, which must not change.
+    //  2.1.3
+    //  2.1.3.1 When rejected a promise must not transition to any other state.
+    //  2.1.3.2 When rejected a promise must have a reason, which must not change.
 
 }
